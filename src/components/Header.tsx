b3a0aec9185fd0d967fac816d8bb59a266af6d9b
John <contactme@johnnyhowell.dev>
2023-03-05 02:11:43 -0600

    bookmark

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import { signIn, signOut, useSession } from 'next-auth/react';
+
+function classNames(...classes: string[]) {
+    return classes.filter(Boolean).join(' ');
+}
+
+export const Header = () => {
+    const { data: sessionData } = useSession();
+
+    return (
+      <div className="navbar bg-primary text-primary-content">
+        <div className="flex-1 pl-5 text-3xl font-bold">
+          {sessionData?.user?.name ? `Notes for ${sessionData.user.name}` : ""}
+        </div>
+      </div>
+    )
+};
